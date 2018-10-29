a20fba5a5f1896afb4e3affff1b0ebd7b11ab9a4
smolarekman <smolarekman@gmail.com>
2018-10-29 11:21:55 +0100

    First Project v0.2: -CRUD,
    To improve:
    -routes,
    -refactoring

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 const express=require('express');
+const bodyParser = require('body-parser');
+
 const app=express();
 
-app.get('/', (req,res)=>{
-    res.send({name:"Marek", surname:"Smolarek Man"});
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// Configuring the database
+const dbConfig = require('C:\\Users\\Administrator\\WebstormProjects\\ProjectIE\\config\\databaseConfig.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT);
+// Require Notes routes
+require('./app/routes/note.routes.js')(app);
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
